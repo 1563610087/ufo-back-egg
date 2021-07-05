b89664bb10c6efd7b28d8e5b1222b96e541ee690
jiange <1563610087@qq.com>
2021-07-05 19:26:41 +0800

    错误异常统一处理

--- app/middleware/error_handler.js
+++ app/middleware/error_handler.js
@@ -5,7 +5,6 @@ return async function errorHandler(ctx, next) {
     } catch (err) {
       // 所有的异常都在 app 上触发一个 error 事件，框架会记录一条错误日志
       ctx.app.emit('error', err, ctx);
-      console.log(11111111111111,err)
 
       const status = err.status || 500;
       // 生产环境时 500 错误的详细错误内容不返回给客户端，因为可能包含敏感信息
--- app/service/website.js
+++ app/service/website.js
@@ -4,13 +4,13 @@ class WebsiteService extends Service {
   async getAllSites() {
     const { app,ctx } = this;
     const {classify_id,classify_type}=ctx.request.body
-    const results=ctx.validate({ classify_id:{
+    ctx.validate({ classify_id:{
       type:'number'
     } },ctx.request.body)
-    // var sql1 = `select * from website_1`;
+    const sql1 = `select * from website_1`;
     // var sql2 = `select * from website_2`;
     // var sql3 = `select * from website_3`;
-    // const [results, metadata] = await app.model.query(sql1);
+    const [results, metadata] = await app.model.query(sql1);
     // const [results, metadata] = await app.model.query(sql2);
     // const [results, metadata] = await app.model.query(sql3);
     return results
